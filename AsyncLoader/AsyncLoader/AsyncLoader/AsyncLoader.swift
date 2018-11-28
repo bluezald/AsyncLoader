8cc3182f4d0ea857b6aacdaeb31cb4db0e3ff1f2
Vincent Bacalso <vincent.bacalso@trustarc.com>
2018-11-28 19:08:25 +0800

    - created initial project for AsyncLoader exercise

---
+++ AsyncLoader/AsyncLoader/AsyncLoader/AsyncLoader.swift
@@ -0,0 +1,11 @@
+//
+//  AsyncLoader.swift
+//  AsyncLoader
+//
+//  Created by Vincent Bacalso on 28/11/2018.
+//  Copyright © 2018 CommandBin. All rights reserved.
+//
+
+import Foundation
+
+// http://pastebin.com/raw/wgkJgazE
